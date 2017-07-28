4e6ef3295669fbbc6d55bc41bac8cf4dd83b93ab
Sebastian Garbarek <sebastian.garbarek@hotmail.co.nz>
2017-07-29 10:03:58 +1200

    Update database manager app class

--- WalgreensDashboardApp/Common/Backend/Models/DatabaseManagerApp.m
+++ WalgreensDashboardApp/Common/Backend/Models/DatabaseManagerApp.m
@@ -10,6 +10,13 @@
 
 @implementation DatabaseManagerApp
 
+- (void)copyInitialDatabase {
+    NSString *sourcePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
+    [[NSFileManager defaultManager] copyItemAtPath:sourcePath toPath:databasePath error:nil];
+}
+
+#pragma mark - Overridden Methods -
+
 - (instancetype)init {
     self = [super init];
     if (self) {
@@ -20,18 +27,14 @@
 
 - (BOOL)openCreateDatabase {
     NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *databasePath = [[documentPaths objectAtIndex:0] stringByAppendingPathComponent:databaseName];
+    databasePath = [[documentPaths objectAtIndex:0] stringByAppendingPathComponent:databaseName];
     
     if (![[NSFileManager defaultManager] fileExistsAtPath:databasePath]) {
-        [self copyInitialDatabaseWith:databasePath];
+        [self copyInitialDatabase];
     }
     
-    return sqlite3_open([databasePath UTF8String], &database);
-}
-
-- (void)copyInitialDatabaseWith:(NSString *)destinationPath {
-    NSString *sourcePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
-    [[NSFileManager defaultManager] copyItemAtPath:sourcePath toPath:destinationPath error:nil];
+    // Super method returns success or failure.
+    return YES;
 }
 
 @end
